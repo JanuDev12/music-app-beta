72ca9cc03c91f4562fe959d787408e3d551e5eca
JanuDev <darkober12@gmail.com>
2024-11-04 16:59:04 -0400

    chore: setup tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Main } from './main';
 import './index.css'
+import { StrictMode } from 'react';
 
 
 const container = document.querySelector('#root');
-const root = createRoot(container);
 
-root.render(<Main />);
+if(container) {
+   const root = createRoot(container);
+   root.render(
+     <StrictMode>
+       <Main />
+     </StrictMode>
+   );
+} else{
+    console.error("Could not find root element")
+}

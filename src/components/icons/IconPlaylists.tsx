9ec998ad0a2ea928ac3d2ff1cc2ebb08a853b953
JanuDev <darkober12@gmail.com>
2024-11-05 12:22:04 -0400

    feat: add styles player

---
+++ src/components/icons/IconPlaylists.tsx
@@ -0,0 +1,37 @@
+function IconPlaylists() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="-mr-2"
+    >
+      <rect
+        width="20"
+        height="20"
+        rx="3"
+        transform="matrix(1 0 0 -1 9.82703 31.3301)"
+        stroke="#898989"
+        stroke-width="2"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.827 11.3301H27.827V11.3301C27.827 9.67322 26.4839 8.33008 24.827 8.33008H14.827C13.1702 8.33008 11.827 9.67322 11.827 11.3301V11.3301Z"
+        stroke="#898989"
+        stroke-width="1.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.827 21.3301H19.827M23.827 21.3301H19.827M19.827 21.3301V17.3301M19.827 21.3301V25.3301"
+        stroke="#898989"
+        stroke-width="2.02054"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default IconPlaylists
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import "./main.css";
 import reactLogo from "./assets/react.svg";
 import FarmLogo from "./assets/logo.png";
 import { Button } from "./components/ui/button";
-import { Github, Search, Sparkle } from "lucide-react";
-import { IconHomeFilled } from "@tabler/icons-react";
+import { EllipsisIcon, Github, MicVocalIcon, MonitorSpeakerIcon, Repeat, Search, Shuffle, Sparkle, Volume1 } from "lucide-react";
+import { IconHeart, IconHomeFilled, IconMicrophone, IconPlayerPlayFilled, IconPlayerSkipBack, IconPlayerSkipBackFilled, IconPlayerSkipForward, IconPlayerSkipForwardFilled } from "@tabler/icons-react";
+import { Slider } from "./components/ui/slider";
+import IconPlaylists from "./components/icons/IconPlaylists";
+import IconQueue from "./components/icons/IconQueue";
+import { Separator } from "./components/ui/separator";
 
 export function Main() {
 
   return (
     <>
-      <div className="flex-grow w-full layout grid">
+      <div className="flex-grow w-full layout grid ">
         <header className="flex gap-7 header p-3 text-white">
           <Button variant="outline" size="sm" className="flex gap-1">
             <Github />
@@ -57,8 +61,59 @@ export function Main() {
           APP
         </main>
         {/* Player */}
-        <div className="player bottom-0 right-0 left-0 bg-red-500 p-3 my-1.5 mr-1.5  rounded-lg">
-          Player
+        <div className="relativd">
+
+        </div>
+        <div className="player relative bottom-0 right-0 left-0  py-3 px-6 my-1.5 mr-1.5  rounded-lg flex items-center text-white justify-between">
+          {/* Players Button  */}
+
+          {/* BLUR */}
+          {/*  <div className="absolute inset-0 backdrop-blur z-auto">
+            
+           </div> */}
+          <div className="flex items-center gap-3">
+            <Button variant="secondary" size="icon" className="rounded-[50%]">
+              <IconPlayerPlayFilled />
+            </Button>
+            <div className="flex gap-3">
+              <IconPlayerSkipForwardFilled />
+              <IconPlayerSkipBackFilled />
+              <Shuffle />
+              <Repeat />
+            </div>
+
+            {/* Slider */}
+            <div className="w-80 flex items-center gap-3 text-xs">
+              <span>1:32</span>
+              <Slider defaultValue={[33]} max={100} step={1} />
+              <span>4:52</span>
+              <div>
+                <Volume1 />
+              </div>
+            </div>
+
+            {/* Card artist */}
+            <div className="flex gap-3">
+              <div className="size-9 bg-slate-400 rounded"></div>
+              <div className="flex flex-col text-xs">
+                <span className="text-sm">Song name</span>
+                <span>Artist name</span>
+              </div>
+            </div>
+          </div>
+
+          {/* Buttons Songs */}
+          <div className="flex items-center gap-5">
+            <IconHeart />
+            <IconPlaylists />
+            <IconQueue />
+            <MonitorSpeakerIcon />
+            <EllipsisIcon />
+
+            <Separator orientation="vertical" color="gray" className="h-10" />
+
+            <MicVocalIcon />
+          </div>
         </div>
       </div>
     </>
